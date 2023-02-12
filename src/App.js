976d71725771946145de975301bd9f1fd661368e
Belal Khaleel <belalkhaleel19@gmail.com>
2023-02-12 18:49:27 +0200

    restored App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React from "react";
 import "./App.css";
-import NewsItem from "./components/News/newsItem";
 
 function App() {
-  return (
-    <div className="App">
-      <NewsItem />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
